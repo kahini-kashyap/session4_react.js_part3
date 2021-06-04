7f6fabab108a97a66ffd9e4dc19c323fb8befe23
Kahini Kashyap <jili@Kahinis-MacBook-Air.local>
2021-06-04 20:15:40 +0530

    session4_part3

--- src/Launches.jsx
+++ src/Launches.jsx
@@ -5,7 +5,7 @@ import './index.css'
 function Launches(props) {
     return (
         <>
-
+            <br/>
             <h3 id="data">{props.name}</h3>
             <h4 id="data">{props.details}</h4>
             <h4 id="data">Launching Date: {props.date}</h4>
